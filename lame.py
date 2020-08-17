7366bfd9fad4d6005869719096efe27c7ad5f374
adirestiawan96 <69594115+adirestiawan96@users.noreply.github.com>
2020-08-17 17:30:23 +0700

    Update lame.py

--- lame.py
+++ lame.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 # enable debugging
@@ -7,9 +7,9 @@ import platform
 
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
+print ("Content-Type: text/plain;charset=utf-8")
+print ()
 
-print "Hello Moon!\n"
-print "Hello Sun!\n\n"
-print "Rendered on:", platform.uname()[1]
+print ("Hello Moon!\n")
+print ("Hello Sun!\n\n")
+print ("Rendered on:", platform.uname()[1])
